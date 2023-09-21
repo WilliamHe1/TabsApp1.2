1a53c53a5946a844d6c81b29c035ee6bb7aa69b1
William He <william_he1@yahoo.com>
2023-09-21 17:04:25 -0400

    Added Structure of tabs and implemented tab 3 (To Do Tab).

---
+++ app.js
@@ -0,0 +1,74 @@
+
+function showTab(tabIndex) {
+    // Hide all content divs
+    const contentDivs = document.querySelectorAll('.content');
+    const tabs = document.querySelectorAll('.tab');
+    contentDivs.forEach((div) => {
+        div.style.display = 'none';
+    });
+    tabs.forEach((tab) => {
+        tab.style.backgroundColor = 'gray';
+        tab.style.color = 'white';
+    });
+
+    // Show the selected content div
+    const selectedContent = document.getElementById(`content-${tabIndex}`);
+    const selectedTab = document.getElementById(`tab-${tabIndex}`)
+    selectedContent.style.display = 'block';
+    selectedTab.style.backgroundColor = 'white';
+    selectedTab.style.color = 'black';
+}
+    const taskList = document.getElementById('taskList');
+    const taskInput = document.getElementById('taskInput');
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+
+        if (taskText === '') {
+            return;
+        }
+
+        const taskItem = document.createElement('li');
+        taskItem.classList.add('task');
+
+        const checkmark = document.createElement('span');
+        checkmark.classList.add('checkmark');
+        checkmark.innerHTML = '-';
+        checkmark.addEventListener('click', toggleTask)
+
+        const label = document.createElement('label');
+        label.textContent = taskText;
+        label.addEventListener('click', toggleTask)
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+        deleteButton.textContent = 'X';
+        deleteButton.addEventListener('click', deleteTask);
+
+        taskItem.appendChild(checkmark);
+        taskItem.appendChild(label);
+        taskItem.appendChild(deleteButton);
+
+        taskList.appendChild(taskItem);
+
+        taskInput.value = '';
+    }
+
+    function toggleTask(event) {
+        const taskItem = event.target.parentElement;
+        const checkmark = taskItem.querySelector('.checkmark');
+        taskItem.classList.toggle('done');
+        if (taskItem.classList.contains('done')) {
+            checkmark.innerHTML = '✓';
+        } else {
+            checkmark.innerHTML = '-';
+        }
+    }
+
+    function deleteTask(event) {
+        const taskItem = event.target.parentElement;
+        taskList.removeChild(taskItem);
+    }
+
+// Show the initial tab (Start by showing tab 1.)
+showTab(0);
